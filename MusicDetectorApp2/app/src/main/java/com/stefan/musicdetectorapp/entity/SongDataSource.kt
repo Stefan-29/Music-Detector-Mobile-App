1d47a86d2c1f6730b6f36c53d5535d2106455562
Stefan-29 <58769705+Stefan-29@users.noreply.github.com>
2023-01-20 13:02:25 +0700

    clearing unused files

--- MusicDetectorApp2/app/src/main/java/com/stefan/musicdetectorapp/entity/SongDataSource.kt
+++
@@ -1,9 +0,0 @@
-//package com.stefan.musicdetectorapp.entity
-//
-//interface SongDataSource {
-//    suspend fun getAllSongs(): Result<List<Song>>
-//    suspend fun getSong(songId: Long): Result<Song>
-//    suspend fun saveSong(song: Song)
-//    suspend fun deleteAllSongs()
-//    suspend fun deleteSongById(songId: Long)
-//}
